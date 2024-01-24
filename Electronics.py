4808660cfa659014018f019d81c535d935d4eff1
itgawri <136715144+itgawri@users.noreply.github.com>
2024-01-24 13:20:28 +0100

    Electronics.py

--- Electronics.py
+++ Electronics.py
@@ -10,20 +10,3 @@ for device in device_list:
   corrected_formatted_device = formatted_device.replace("@", "a")
   formatted_list.append(corrected_formatted_device)
 print(formatted_list)
-
-# PL----------------------------------------------------------------------
-
-dane = "14c@lowy l@top,699|16c@lowy l@ptop,999|sm@rtfon,1099|t@blet,499|komputer do gier,1999"
-lista_urzadzen = dane.split("|")
-sformatowana_lista = []
-
-for urzadzenie in lista_urzadzen:
-    informacje_o_urzadzeniu = urzadzenie.split(",")
-    nazwa = informacje_o_urzadzeniu[0]
-    cena = int(informacje_o_urzadzeniu[1])
-    nowa_cena = int(cena * 1.1)
-    sformatowane_urzadzenie = f"Nazwa urządzenia: {nazwa}, Cena urządzenia: {nowa_cena} zł"
-    poprawione_sformatowane_urzadzenie = sformatowane_urzadzenie.replace("@", "a")
-    sformatowana_lista.append(poprawione_sformatowane_urzadzenie)
-
-print(sformatowana_lista)
